466781240251a196153e960b5a7ba01dce0c467e
Jack Kallend <Kallendo96@live.co.uk>
2023-03-27 16:41:34 +0100

    fix(vercel build): fix eslint issue in project build step

--- apps/gk-next/src/app/page.tsx
+++ apps/gk-next/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
     return (
         <h1 className="container p-10 text-4xl font-bold bg-brand-primary text-brand-white">
-            Garrett Kitchens - Croydon's Best Kitchens
+            Garrett Kitchens
         </h1>
     )
 }
