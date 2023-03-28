8cc0c6d988f0eda7f29774863430e4542fa315b9
Jack Kallend <Kallendo96@live.co.uk>
2023-03-28 21:16:17 +0100

    feat(header): finished header

--- apps/gk-next/src/app/components/header/Header.tsx
+++ apps/gk-next/src/app/components/header/Header.tsx
@@ -1,53 +1,35 @@
 'use client'
 
 import { Fragment, useState } from 'react'
+import Image from 'next/image'
 import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react'
-import {
-    ArrowPathIcon,
-    Bars3Icon,
-    ChartPieIcon,
-    CursorArrowRaysIcon,
-    FingerPrintIcon,
-    SquaresPlusIcon,
-    XMarkIcon,
-} from '@heroicons/react/24/outline'
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
-const products = [
+const kitchens = [
     {
-        name: 'Analytics',
-        description: 'Get a better understanding of your traffic',
-        href: '#',
-        icon: ChartPieIcon,
+        name: 'Modern Kitchens',
+        href: '/modern-kitchens',
     },
     {
-        name: 'Engagement',
-        description: 'Speak directly to your customers',
-        href: '#',
-        icon: CursorArrowRaysIcon,
-    },
-    {
-        name: 'Security',
-        description: 'Your customers’ data will be safe and secure',
-        href: '#',
-        icon: FingerPrintIcon,
-    },
-    {
-        name: 'Integrations',
-        description: 'Connect with third-party tools',
-        href: '#',
-        icon: SquaresPlusIcon,
-    },
-    {
-        name: 'Automations',
-        description: 'Build strategic funnels that will convert',
-        href: '#',
-        icon: ArrowPathIcon,
+        name: 'Traditional Kitchens',
+        href: '/traditional-kitchens',
     },
 ]
-const callsToAction = [
-    { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-    { name: 'Contact sales', href: '#', icon: PhoneIcon },
+
+const bookings = [
+    {
+        name: 'Showroom Appointment',
+        href: '/showroom-appointment',
+    },
+    {
+        name: 'Home Survey',
+        href: '/home-survey',
+    },
+    {
+        name: 'Advice Consultation',
+        href: '/advice-consultation',
+    },
 ]
 
 function classNames(...classes) {
@@ -64,13 +46,9 @@ export default function Header() {
                 aria-label="Global"
             >
                 <div className="flex lg:flex-1">
-                    <a href="#" className="-m-1.5 p-1.5">
-                        <span className="sr-only">Your Company</span>
-                        <img
-                            className="w-auto h-8"
-                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                            alt=""
-                        />
+                    <a href="/" className="-m-1.5 p-1.5">
+                        <span className="sr-only">Garrett Kitchens</span>
+                        <Image width="175" height="66" src="/assets/logos/logo.png" alt="Logo" />
                     </a>
                 </div>
                 <div className="flex lg:hidden">
@@ -86,13 +64,12 @@ export default function Header() {
                 <Popover.Group className="hidden lg:flex lg:gap-x-12">
                     <Popover className="relative">
                         <Popover.Button className="flex items-center text-sm font-semibold leading-6 text-gray-900 gap-x-1">
-                            Product
+                            Kitchens
                             <ChevronDownIcon
                                 className="flex-none w-5 h-5 text-gray-400"
                                 aria-hidden="true"
                             />
                         </Popover.Button>
-
                         <Transition
                             as={Fragment}
                             enter="transition ease-out duration-200"
@@ -102,19 +79,13 @@ export default function Header() {
                             leaveFrom="opacity-100 translate-y-0"
                             leaveTo="opacity-0 translate-y-1"
                         >
-                            <Popover.Panel className="absolute z-10 w-screen max-w-md mt-3 overflow-hidden bg-white shadow-lg -left-8 top-full rounded-3xl ring-1 ring-gray-900/5">
+                            <Popover.Panel className="absolute z-10 w-screen max-w-sm mt-3 overflow-hidden bg-white shadow-lg -left-8 top-full rounded-3xl ring-1 ring-gray-900/5">
                                 <div className="p-4">
-                                    {products.map((item) => (
+                                    {kitchens.map((item) => (
                                         <div
                                             key={item.name}
                                             className="relative flex items-center p-4 text-sm leading-6 rounded-lg group gap-x-6 hover:bg-gray-50"
                                         >
-                                            <div className="flex items-center justify-center flex-none rounded-lg h-11 w-11 bg-gray-50 group-hover:bg-white">
-                                                <item.icon
-                                                    className="w-6 h-6 text-gray-600 group-hover:text-indigo-600"
-                                                    aria-hidden="true"
-                                                />
-                                            </div>
                                             <div className="flex-auto">
                                                 <a
                                                     href={item.href}
@@ -123,45 +94,62 @@ export default function Header() {
                                                     {item.name}
                                                     <span className="absolute inset-0" />
                                                 </a>
-                                                <p className="mt-1 text-gray-600">
-                                                    {item.description}
-                                                </p>
                                             </div>
                                         </div>
-                                    ))}
-                                </div>
-                                <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-                                    {callsToAction.map((item) => (
-                                        <a
-                                            key={item.name}
-                                            href={item.href}
-                                            className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
-                                        >
-                                            <item.icon
-                                                className="flex-none w-5 h-5 text-gray-400"
-                                                aria-hidden="true"
-                                            />
-                                            {item.name}
-                                        </a>
                                     ))}
                                 </div>
                             </Popover.Panel>
                         </Transition>
                     </Popover>
-
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                        Features
+                    <a href="/brochure" className="text-sm font-semibold leading-6 text-gray-900">
+                        Brochure
                     </a>
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                        Marketplace
+                    <a href="/projects" className="text-sm font-semibold leading-6 text-gray-900">
+                        Our Projects
                     </a>
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                        Company
-                    </a>
+                    <Popover className="relative">
+                        <Popover.Button className="flex items-center text-sm font-semibold leading-6 text-gray-900 gap-x-1">
+                            Bookings
+                            <ChevronDownIcon
+                                className="flex-none w-5 h-5 text-gray-400"
+                                aria-hidden="true"
+                            />
+                        </Popover.Button>
+                        <Transition
+                            as={Fragment}
+                            enter="transition ease-out duration-200"
+                            enterFrom="opacity-0 translate-y-1"
+                            enterTo="opacity-100 translate-y-0"
+                            leave="transition ease-in duration-150"
+                            leaveFrom="opacity-100 translate-y-0"
+                            leaveTo="opacity-0 translate-y-1"
+                        >
+                            <Popover.Panel className="absolute z-10 w-screen max-w-sm mt-3 overflow-hidden bg-white shadow-lg -left-8 top-full rounded-3xl ring-1 ring-gray-900/5">
+                                <div className="p-4">
+                                    {bookings.map((item) => (
+                                        <div
+                                            key={item.name}
+                                            className="relative flex items-center p-4 text-sm leading-6 rounded-lg group gap-x-6 hover:bg-gray-50"
+                                        >
+                                            <div className="flex-auto">
+                                                <a
+                                                    href={item.href}
+                                                    className="block font-semibold text-gray-900"
+                                                >
+                                                    {item.name}
+                                                    <span className="absolute inset-0" />
+                                                </a>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </Popover.Panel>
+                        </Transition>
+                    </Popover>
                 </Popover.Group>
                 <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                        Log in <span aria-hidden="true">&rarr;</span>
+                    <a href="/contact-us" className="text-sm font-semibold leading-6 text-gray-900">
+                        Contact Us <span aria-hidden="true">&rarr;</span>
                     </a>
                 </div>
             </nav>
@@ -174,12 +162,13 @@ export default function Header() {
                 <div className="fixed inset-0 z-10" />
                 <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full px-6 py-6 overflow-y-auto bg-white sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
-                        <a href="#" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Your Company</span>
-                            <img
-                                className="w-auto h-8"
-                                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                                alt=""
+                        <a href="/" className="-m-1.5 p-1.5">
+                            <span className="sr-only">Garrett Kitchens</span>
+                            <Image
+                                width="175"
+                                height="66"
+                                src="/assets/logos/logo.png"
+                                alt="Logo"
                             />
                         </a>
                         <button
@@ -198,7 +187,7 @@ export default function Header() {
                                     {({ open }) => (
                                         <>
                                             <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 hover:bg-gray-50">
-                                                Product
+                                                Kitchens
                                                 <ChevronDownIcon
                                                     className={classNames(
                                                         open ? 'rotate-180' : '',
@@ -208,7 +197,7 @@ export default function Header() {
                                                 />
                                             </Disclosure.Button>
                                             <Disclosure.Panel className="mt-2 space-y-2">
-                                                {[...products, ...callsToAction].map((item) => (
+                                                {[...kitchens].map((item) => (
                                                     <Disclosure.Button
                                                         key={item.name}
                                                         as="a"
@@ -223,30 +212,52 @@ export default function Header() {
                                     )}
                                 </Disclosure>
                                 <a
-                                    href="#"
+                                    href="/brochure"
                                     className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg hover:bg-gray-50"
                                 >
-                                    Features
+                                    Brochure
                                 </a>
                                 <a
-                                    href="#"
+                                    href="/projects"
                                     className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg hover:bg-gray-50"
                                 >
-                                    Marketplace
+                                    Projects
                                 </a>
-                                <a
-                                    href="#"
-                                    className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg hover:bg-gray-50"
-                                >
-                                    Company
-                                </a>
+                                <Disclosure as="div" className="-mx-3">
+                                    {({ open }) => (
+                                        <>
+                                            <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 hover:bg-gray-50">
+                                                Bookings
+                                                <ChevronDownIcon
+                                                    className={classNames(
+                                                        open ? 'rotate-180' : '',
+                                                        'h-5 w-5 flex-none',
+                                                    )}
+                                                    aria-hidden="true"
+                                                />
+                                            </Disclosure.Button>
+                                            <Disclosure.Panel className="mt-2 space-y-2">
+                                                {[...bookings].map((item) => (
+                                                    <Disclosure.Button
+                                                        key={item.name}
+                                                        as="a"
+                                                        href={item.href}
+                                                        className="block py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 rounded-lg hover:bg-gray-50"
+                                                    >
+                                                        {item.name}
+                                                    </Disclosure.Button>
+                                                ))}
+                                            </Disclosure.Panel>
+                                        </>
+                                    )}
+                                </Disclosure>
                             </div>
                             <div className="py-6">
                                 <a
-                                    href="#"
+                                    href="/contact-us"
                                     className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                                 >
-                                    Log in
+                                    Contact Us
                                 </a>
                             </div>
                         </div>
