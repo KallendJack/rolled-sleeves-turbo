bcf32a391539d7029b48ab63bff7db0c9b4c6085
paulc112 <paulmcookson@gmail.com>
2023-04-03 21:01:35 +0100

    refactor(homepage): new homepage block - added new image for testing on brochure block

--- apps/gk-next/src/app/page.tsx
+++ apps/gk-next/src/app/page.tsx
@@ -7,6 +7,24 @@ export const metadata = {
     title: 'Home | Garrett Kitchens',
 }
 
+const features = [
+    {
+        name: 'Quality Kitchens',
+        description:
+            'Commodo nec sagittis tortor mauris sed. Turpis tortor quis scelerisque diam id accumsan nullam tempus. Pulvinar etiam lacus volutpat eu. Phasellus praesent ligula sit faucibus.',
+    },
+    {
+        name: 'Easy Process',
+        description:
+            'Pellentesque enim a commodo malesuada turpis eleifend risus. Facilisis donec placerat sapien consequat tempor fermentum nibh.',
+    },
+    {
+        name: 'Customer Service',
+        description:
+            'Pellentesque sit elit congue ante nec amet. Dolor aenean curabitur viverra suspendisse iaculis eget. Nec mollis placerat ultricies euismod ut condimentum.',
+    },
+]
+
 const collections = [
     {
         name: 'Winslow',
@@ -101,9 +119,41 @@ export default function Page() {
                 </div>
             </section>
 
+            <section className="bg-white py-24 sm:py-32">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div className="mx-auto max-w-2xl lg:text-center">
+                        <h2 className="text-base font-semibold leading-7 text-brand-primary">
+                            Why choose us
+                        </h2>
+                        <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                            What makes Garrett Kitchens different?
+                        </p>
+                        <p className="mt-6 text-lg leading-8 text-gray-600">
+                            Quis tellus eget adipiscing convallis sit sit eget aliquet quis.
+                            Suspendisse eget egestas a elementum pulvinar et feugiat blandit at. In
+                            mi viverra elit nunc.
+                        </p>
+                    </div>
+                    <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
+                        <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
+                            {features.map((feature) => (
+                                <div key={feature.name} className="flex flex-col">
+                                    <dt className="flex text-center items-center gap-x-3 text-base font-semibold leading-7 text-gray-900">
+                                        {feature.name}
+                                    </dt>
+                                    <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-600">
+                                        <p className="flex-auto">{feature.description}</p>
+                                    </dd>
+                                </div>
+                            ))}
+                        </dl>
+                    </div>
+                </div>
+            </section>
+
             <section
                 aria-labelledby="collection-heading"
-                className="max-w-3xl px-4 py-24 mx-auto sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8"
+                className="max-w-3xl px-4 pb-24 mx-auto sm:px-6 sm:pb-32 lg:max-w-7xl lg:px-8"
             >
                 <h2
                     id="collection-heading"
@@ -229,7 +279,7 @@ export default function Page() {
                                 <div className="absolute inset-0 object-cover w-full h-full shadow-2xl xl:w-1/2 rounded-2xl bg-brand-primaryBgDark">
                                     <Image
                                         fill
-                                        src="/assets/home/hero/hero.png"
+                                        src="/assets/home/kitchen-brochure.png"
                                         alt="Brochure"
                                         className="object-cover rounded-2xl"
                                     />
