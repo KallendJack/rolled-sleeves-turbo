643a71722179d9f4f60f6f5d50934e2168e5dfba
paulc112 <paulmcookson@gmail.com>
2023-04-26 21:59:29 +0100

    refactor(changed logo secrtion on homepage): changed logo section

--- apps/gk-next/src/app/page.tsx
+++ apps/gk-next/src/app/page.tsx
@@ -265,44 +265,47 @@ export default function Page() {
                 </div>
             </section>
 
-            <section className="w-full border-gray-200 border-y text-center">
-                <h4 className="pt-6 font-semibold">We work with these trusted brands</h4>
-
-                <div className="flex flex-row justify-between gap-4 py-8 items-center  mx-auto md:px-6 lg:max-w-7xl lg:px-8 max-w-4xl px-4">
-                    <div className="flex gap-2 items-center">
-                        <img className="w-5 h-5 bg-red-400 rounded-full" src="" alt="" />
-
-                        <p className="flex items-center text-base font-semibold leading-7 text-center text-gray-600 text-md md:text-lg gap-x-3">
-                            Blum
-                        </p>
-                    </div>
-                    <div className="flex gap-2 items-center">
-                        <img className="w-5 h-5 bg-red-400 rounded-full" src="" alt="" />
-
-                        <p className="flex items-center text-base font-semibold leading-7 text-center text-gray-600 text-md md:text-lg gap-x-3">
-                            Blum
-                        </p>
-                    </div>
-                    <div className="flex gap-2 items-center">
-                        <img className="w-5 h-5 bg-red-400 rounded-full" src="" alt="" />
-
-                        <p className="flex items-center text-base font-semibold leading-7 text-center text-gray-600 text-md md:text-lg gap-x-3">
-                            Blum
-                        </p>
-                    </div>
-                    <div className="flex gap-2 items-center">
-                        <img className="w-5 h-5 bg-red-400 rounded-full" src="" alt="" />
-
-                        <p className="flex items-center text-base font-semibold leading-7 text-center text-gray-600 text-md md:text-lg gap-x-3">
-                            Blum
-                        </p>
-                    </div>
-                    <div className="flex gap-2 items-center">
-                        <img className="w-5 h-5 bg-red-400 rounded-full" src="" alt="" />
-
-                        <p className="flex items-center text-base font-semibold leading-7 text-center text-gray-600 text-md md:text-lg gap-x-3">
-                            Blum
-                        </p>
+            <section className="bg-white py-12 md:py-32">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
+                        Trusted by the worlds most innovative teams
+                    </h2>
+                    <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+                        <Image
+                            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                            src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
+                            alt="Transistor"
+                            width={158}
+                            height={48}
+                        />
+                        <Image
+                            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                            src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
+                            alt="Reform"
+                            width={158}
+                            height={48}
+                        />
+                        <Image
+                            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                            src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
+                            alt="Tuple"
+                            width={158}
+                            height={48}
+                        />
+                        <Image
+                            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+                            src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
+                            alt="SavvyCal"
+                            width={158}
+                            height={48}
+                        />
+                        <Image
+                            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+                            src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
+                            alt="Statamic"
+                            width={158}
+                            height={48}
+                        />
                     </div>
                 </div>
             </section>
