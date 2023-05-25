bbb14d8f3b295034649f3a2aa27d6018e7538100
Jack Kallend <Kallendo96@live.co.uk>
2023-05-25 18:31:24 +0100

    refactor(brochure): fix build and page url

--- apps/gk-next/src/app/request-a-free-brochure/page.tsx
+++ apps/gk-next/src/app/request-a-free-brochure/page.tsx
@@ -5,33 +5,33 @@ import { Button } from 'components/button/Button'
 import Input from 'components/input/Input'
 
 export const metadata = {
-    title: 'Worktops | Garrett Kitchens',
+    title: 'Request a Free Brochure | Garrett Kitchens',
 }
 
 export default function Page() {
     return (
         <>
-            <section className="py-lg bg-white flex w-full justify-center">
-                <div className="w-full md:max-w-4xl flex px-4 mx-auto md:px-6 lg:max-w-7xl lg:px-8">
-                    <div className="flex w-full flex-col md:flex-row gap-6 lg:gap-8 border border-gray-200 shadow-lg">
+            <section className="flex justify-center w-full bg-white py-lg">
+                <div className="flex w-full px-4 mx-auto md:max-w-4xl md:px-6 lg:max-w-7xl lg:px-8">
+                    <div className="flex flex-col w-full gap-6 border border-gray-200 shadow-lg md:flex-row lg:gap-8">
                         <Image
                             width={500}
                             height={400}
                             src="/assets/home/hero/hero.png"
                             alt="image"
-                            className="w-full md:w-1/2 relative"
+                            className="relative w-full md:w-1/2"
                         ></Image>
 
                         <div className="w-full p-4 md:py-8 md:w-1/2">
                             <h2
                                 id="collection-heading"
-                                className="text-2xl tracking-tight text-gray-900 mb-4 lg:text-3xl"
+                                className="mb-4 text-2xl tracking-tight text-gray-900 lg:text-3xl"
                             >
                                 Get Our Free Kitchen Brochure
                             </h2>
 
                             <p>Fill in your details below to download our kitchen brochure.</p>
-                            <p>Don't forget to save a copy!</p>
+                            <p>Don&apos;t forget to save a copy!</p>
 
                             <form className="flex flex-col gap-4 mt-4 md:w-4/5 lg:w-3/5">
                                 <div>
@@ -39,7 +39,7 @@ export default function Page() {
                                         htmlFor="first-name"
                                         className="block text-sm font-semibold leading-6 text-gray-900"
                                     >
-                                        What's your email address?
+                                        What&apos;s your email address?
                                     </label>
                                     <div className="mt-2.5">
                                         <Input type="email" autoComplete="email" />
@@ -70,7 +70,7 @@ export default function Page() {
                                     </div>
                                 </div>
 
-                                <div className="flex mt-4 border-gray-900/10 w-full">
+                                <div className="flex w-full mt-4 border-gray-900/10">
                                     <Button type="submit" className="w-full">
                                         Download Brochure
                                     </Button>
